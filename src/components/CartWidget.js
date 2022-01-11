ae4ff8ea7545d3b0244bfe0dcd38b419d62dad25
franco.buonvi <franco.buonvi@gmail.com>
2022-01-11 09:46:51 -0300

    clase 11- Buonvicino

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -2,16 +2,15 @@
 import {useContexto} from './context.js'
 
 
-
 function CartWidget(){
 
     const { qty_cart } = useContexto()
 
     return (
-    <>
+    <div className='cart_icon'>
         <span className="material-icons">shopping_cart</span>
-        <p>{qty_cart}</p>
-    </>);
+        <p className='qty_cart'>{qty_cart}</p>
+    </div>);
 
 }
 
